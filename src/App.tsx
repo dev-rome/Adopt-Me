4e49e7a2d4207511b060c08b8504b052668efc03
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-05-05 16:04:47 -0400

    add query client and fetch pet tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./pages/Details";
 import SearchParams from "./components/SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 function App() {
   return (
     <>
       <main>
         <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
         </BrowserRouter>
       </main>
     </>
---
+++ src/services/fetchPet.tsx
@@ -0,0 +1,12 @@
+const fetchPet = async ({ queryKey }: { queryKey: any[] }) => {
+    const id = queryKey[1];
+    const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
+
+    if (!apiRes.ok) {
+        throw new Error("Something went wrong");
+    }
+
+    return apiRes.json();
+};
+
+export default fetchPet;
