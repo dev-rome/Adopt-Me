4a484673b7ace91ed5da629ee0f953ade9f094c7
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-05-05 15:41:30 -0400

    add useParams to get id for pet, add link to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./pages/Details";
 import SearchParams from "./components/SearchParams";
 
@@ -8,7 +8,9 @@ function App() {
     <>
       <main>
         <BrowserRouter>
-          <h1>Adopt Me!</h1>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
           <Routes>
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
