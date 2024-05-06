324858fc8bdb720def136336800a9b143088b1ab
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-05-05 23:06:18 -0400

    add carousel to details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "../services/fetchPet";
+import Carousel from "../components/Carousel";
 
 const Details = () => {
   const { id } = useParams();
@@ -18,6 +19,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <h1>{pet.name}</h1>
       <h2>
         {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
